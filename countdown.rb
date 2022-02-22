ba967fcd82bfb0aa4245014fcd6a5f17ae6cbee0
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-21 21:57:01 -0500

    commit tree 2ebe69854cc306cf53fd85ad1a746f92e166a798

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown int 
+    while int > 0 
+        # sleep(1)
+        int -= 1  
+        puts "#{int + 1} SECOND(S)!"
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep int 
+    while int > 0 
+        sleep(1)
+        int -= 1  
+        puts "#{int + 1} SECOND(S)!"
+    end
+    "HAPPY NEW YEAR!"
+end
+
+countdown 10 
